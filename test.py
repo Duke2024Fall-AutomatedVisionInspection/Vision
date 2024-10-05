63d571214cd79710c62cbe2720e74e04ae96dff1
rhit-yinz1 <75374376+rhit-yinz1@users.noreply.github.com>
2024-10-04 23:55:39 -0400

    initial setup for yolo vison

---
+++ test.py
@@ -0,0 +1,24 @@
+import cv2
+from ultralytics import YOLO
+import numpy as np
+
+
+cap = cv2.VideoCapture("/Users/theoy/Movies/GoPro/GX010395.MP4")
+# ret, frame = cap.read()
+# cv2.imshow("Img", frame)
+# cv2.waitKey(0)
+model = YOLO("yolov8m.pt")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    results = model(frame)
+    result = result[0]
+    print(result)
+    cv2.imshow("Img", frame)
+    key = cv2.waitKey(0)
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
